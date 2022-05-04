7796ca4df881d54809e07e29a8445e63a82f55db
BeauteousJade <pby2196995023@163.com>
2022-05-04 18:51:59 +0800

    更新io代码

--- code/io_simple_test.cpp
+++ code/io_simple_test.cpp
@@ -2,18 +2,28 @@
 // Created by pby on 2022/5/4.
 //
 
+#include <iostream>
 #include <fstream>
+#include <istream>
+#include <string>
 
 using namespace std;
 
 int main() {
 
+    string file = "../file/test.txt";
     fstream outFile;
-    outFile.open("../file/test.txt");
+    outFile.open(file);
     outFile << 1 << endl;
     outFile << "123" << endl;
     outFile << 12.3 << endl;
     outFile << "jade" << endl;
-
     outFile.close();
+
+    ifstream in;
+    in.open(file);
+    char line[60];
+    while (in.getline(line, 60)) {
+        cout << "line = " << line << endl;
+    }
 }
